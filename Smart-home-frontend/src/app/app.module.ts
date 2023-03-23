76743ee4d5eb473961982b61b5f8d241c4bbc10b
Uros <prijovic.uros13@gmail.com>
2023-03-23 16:41:06 +0100

    Added NgRx and example of directory hierarchy

--- Smart-home-frontend/src/app/app.module.ts
+++ Smart-home-frontend/src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { SharedModule } from "./shared/shared.module";
+import { HttpClientModule } from "@angular/common/http";
+import { StoreModule } from "@ngrx/store";
+import * as fromApp from "./store/app.reducer";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment.prod";
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
@@ -14,10 +20,15 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
+    HttpClientModule,
+    StoreModule.forRoot(fromApp.appReducer),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
     SharedModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
