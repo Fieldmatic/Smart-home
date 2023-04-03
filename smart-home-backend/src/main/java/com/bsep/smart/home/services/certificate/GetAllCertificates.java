f311a2af7a2b445f1484d44a727f19d186095fb3
Ivana Stevanovic <stevanovic.sw69.2019@uns.ac.rs>
2023-04-03 19:09:47 +0200

    fix get all certificates, add revoked field

--- smart-home-backend/src/main/java/com/bsep/smart/home/services/certificate/GetAllCertificates.java
+++ smart-home-backend/src/main/java/com/bsep/smart/home/services/certificate/GetAllCertificates.java
@@ -1,21 +1,22 @@
 package com.bsep.smart.home.services.certificate;
 
 import com.bsep.smart.home.dto.response.CertificateResponse;
-import com.bsep.smart.home.model.CSR;
-import com.bsep.smart.home.model.CSRStatus;
 import com.bsep.smart.home.model.Person;
 import com.bsep.smart.home.repository.PersonRepository;
 import com.bsep.smart.home.services.keystore.LoadX509Certificate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
+import java.security.cert.CRLException;
 import java.security.cert.CertPathValidatorException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -24,19 +25,19 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class GetAllCertificates {
     private final PersonRepository personRepository;
-    private final IsCertificateValid isCertificateValid;
+    private final CheckCertificateValidity checkCertificateValidity;
     private final LoadX509Certificate loadX509Certificate;
-    private final IsCertificateChainValid isCertificateChainValid;
+    private final IsCertificateRevoked isCertificateRevoked;
 
-    public List<CertificateResponse> execute() throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException, CertPathValidatorException, InvalidAlgorithmParameterException, CertificateException {
+
+    public List<CertificateResponse> execute() throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException, CertPathValidatorException, InvalidAlgorithmParameterException, CertificateException, IOException, CRLException, InvalidKeySpecException {
         List<CertificateResponse> certificateResponses = new ArrayList<>();
         for (Person person : personRepository.findAll()) {
-            CSR csr = person.getCsr();
-            if (!Objects.isNull(csr) && csr.getStatus().equals(CSRStatus.ACCEPTED)) {
-                boolean certificateValid = isCertificateValid.execute(person.getEmail());
-                boolean certificateChainValid = isCertificateChainValid.execute(person.getEmail());
+            if (!Objects.isNull(loadX509Certificate.execute(person.getEmail()))) {
+                boolean valid = checkCertificateValidity.execute(person.getEmail());
+                boolean revoked = isCertificateRevoked.execute(person.getEmail());
                 X509Certificate certificate = loadX509Certificate.execute(person.getEmail());
-                certificateResponses.add(new CertificateResponse(person.getEmail(), extractCommonNameFromPrincipalName(certificate.getSubjectX500Principal().getName()), extractCommonNameFromPrincipalName(certificate.getIssuerX500Principal().getName()), certificate.getNotBefore(), certificate.getNotAfter(), certificateValid && certificateChainValid));
+                certificateResponses.add(new CertificateResponse(person.getEmail(), extractCommonNameFromPrincipalName(certificate.getSubjectX500Principal().getName()), extractCommonNameFromPrincipalName(certificate.getIssuerX500Principal().getName()), certificate.getNotBefore(), certificate.getNotAfter(), valid, revoked));
             }
         }
         return certificateResponses;
