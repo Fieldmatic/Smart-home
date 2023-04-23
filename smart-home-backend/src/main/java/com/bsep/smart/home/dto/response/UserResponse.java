15a0bc2ad7f80ef127b1095d577f35c611694c9a
Mico Milic <milicftn@gmail.com>
2023-04-23 11:09:41 +0200

    id missing in response fix

--- smart-home-backend/src/main/java/com/bsep/smart/home/dto/response/UserResponse.java
+++ smart-home-backend/src/main/java/com/bsep/smart/home/dto/response/UserResponse.java
@@ -7,12 +7,14 @@ import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserResponse {
+    UUID id;
     @Email
     @NotEmpty
     String email;
